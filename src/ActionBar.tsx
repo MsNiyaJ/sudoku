46048450aede2fd1b61300487cd7faeb7788a01f
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-01-13 23:42:42 -0500

    new app w/ typescript, added a navbar

---
+++ src/ActionBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DifficultyList } from './DifficultyList';
+
+const ActionBar = () => {
+  return (
+    <div className="action-bar">
+      <p>Difficulty: Easy/Medium/Hard</p>
+      <p>00:00</p>
+    </div>
+  );
+};
+
+export default ActionBar;
---
+++ src/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar">
+        <h1 className="logo-title">Sudoku</h1>
+        <div className="nav-links">
+          <a href="/" className="nav-item">
+            How to Play
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
